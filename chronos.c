97f4349ce3612dcdc563b58fadf19d9ab21bb1f6
BirminghamAcademics <b.bentley30@OUTLOOK.COM>
2025-12-30 17:19:58 -0500

    Update chronos.c

--- chronos.c
+++ chronos.c
@@ -1,30 +1,27 @@
 #include <tice.h>
 #include <fileioc.h>
-#include <string.h>
 
 #define EXEC ((void*)0xD40000)
 
 typedef struct {
   char magic[8];
   uint8_t core;
-  uint16_t size;
 } ChronosHeader;
 
-void loadGame(void){
-  ti_var_t v=ti_Open("GAME","r");
-  if(!v) return;
+void runGame(void){
+  ti_var_t g = ti_Open("GAME","r");
+  if(!g) return;
 
   ChronosHeader h;
-  ti_Read(&h,sizeof(h),1,v);
+  ti_Read(&h,sizeof(h),1,g);
+  ti_Close(g);
 
   const char* core =
     h.core==1 ? "CORE_GB" :
     h.core==3 ? "CORE_SMS" :
     "CORE_GG";
 
-  ti_Close(v);
-
-  ti_var_t c=ti_Open(core,"r");
+  ti_var_t c = ti_Open(core,"r");
   ti_Read(EXEC,ti_GetSize(c),1,c);
   ti_Close(c);
 
@@ -33,9 +30,7 @@ void loadGame(void){
 
 int main(void){
   os_ClrHome();
-  os_PutStrFull("CHRONOS\n\n[ENTER] Run Game");
-
-  while(1)
-    if(os_GetCSC()==sk_Enter)
-      loadGame();
+  os_PutStrFull("CHRONOS\n[ENTER] Run");
+  while(os_GetCSC()!=sk_Enter);
+  runGame();
 }
