6c57f17fc3462987787cef383f425e9dc03ef51b
BirminghamAcademics <b.bentley30@OUTLOOK.COM>
2025-12-30 17:15:00 -0500

    Update cores.c

--- cores.c
+++ cores.c
@@ -1,22 +1,45 @@
 #include <tice.h>
 #include <graphx.h>
+#include <string.h>
 
 #ifndef CORE_ID
-#error CORE_ID not defined
+#error define CORE_ID
 #endif
+
+uint8_t rom[0x8000];
+
+void zx7_decompress(uint8_t* src,uint8_t* dst){
+  memcpy(dst,src,0x8000); // placeholder
+}
+
+void save_state(void){
+  // dump RAM to AppVar (stub)
+}
+
+void load_state(void){}
+
+void emulate_frame(void){
+  // CPU + PPU per frame
+}
 
 int main(void){
   gfx_Begin();
   gfx_FillScreen(0);
 
 #if CORE_ID==1
-  gfx_PrintStringXY("GAME BOY CORE",20,20);
+  gfx_PrintStringXY("GB CORE",10,10);
 #elif CORE_ID==3
-  gfx_PrintStringXY("SMS CORE",20,20);
+  gfx_PrintStringXY("SMS CORE",10,10);
+#elif CORE_ID==4
+  gfx_PrintStringXY("GG CORE",10,10);
 #endif
 
   while(1){
-    if(os_GetCSC()==sk_Clear) break;
+    emulate_frame();
+
+    uint8_t k=os_GetCSC();
+    if(k==sk_Mode) save_state();
+    if(k==sk_Clear) break;
   }
 
   gfx_End();
